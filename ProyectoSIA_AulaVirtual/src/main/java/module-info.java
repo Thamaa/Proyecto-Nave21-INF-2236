e3e5a8b5115ac3ec2922bb7024572ced9d98a0d2
Thamaa <91970252+Thamaa@users.noreply.github.com>
2024-10-06 23:20:50 -0300

    Delete ProyectoSIA_AulaVirtual directory

--- ProyectoSIA_AulaVirtual/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.proyectosia_aulavirtual {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.proyectosia_aulavirtual to javafx.fxml;
-    exports com.example.proyectosia_aulavirtual;
-}
